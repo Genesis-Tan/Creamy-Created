9c94da6688a57101d37fa7ae0b56da477eb62c44
Genesis-Tan <genesischarlestan@gmail.com>
2025-07-01 20:00:39 +0800

    pseudo code comments

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,8 +2,17 @@ const express = require('express');
 const app = express();
 
 app.get('/Menu', (req, res) => {
-  //fetch db info
-  console.log('Burger');
+  //initialize DB instance
+  //Read MENU table from DB
+  //Place into JSON string
+  //Set 'res' to JSON string
+});
+
+app.post('/Checkout', (req, res) => {
+  //Unwrap 'req'
+  //Write to DB table "orders"
+  //Send JSON of Order to KitchenDisplay
+  // Set 'res' to 'Success'
 });
 
 app.listen(3000);
